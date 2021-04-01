189a7a5f125cbc8e1bb5e11c5f85bbdb1d1b4563
2030575221 <2030575221@qq.com>
2021-04-01 16:57:23 +0800

    add

---
+++ src/renderer/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+      <el-button type="danger">测试element</el-button>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'vuedemo'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
